d37f7f45c5ef0d86a16bbd6f0df8c27854f483e1
JoeTac <1260694+JoeTac@users.noreply.github.com>
2020-05-05 16:10:47 -0400

    Update sort-object.pipe.ts

--- pipes/sort-object.pipe.ts
+++ pipes/sort-object.pipe.ts
@@ -1,21 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * Usage: sortObject: { key: 'property'|'subObject.property', asc: true|false }
+ */
+
 @Pipe({
   name: 'sortObject'
 })
 export class SortObjectPipe implements PipeTransform {
+  transform(object: any, properties:string): any {
+    let key = properties['key'];
+    let asc = properties['asc'];
 
-  transform(object: any, property:string): any {
+    if ( !key ) return object;
+    if ( !asc ) asc = true;
+
     return object.sort((a,b) => {
-      if ( a[property]<b[property] ) {
-        return -1;
-      }
-      else if ( a[property]>b[property] ) {
-        return 1;
+      let aa = this.extract(a, key);
+      let bb = this.extract(b, key);
+
+      if ( asc ) {
+        return aa>bb ? 1:-1;
       }
       else {
-        return 0;
+        return aa>bb ? -1:1;
       }
     });
+  }
+
+  private extract(obj, key:string):string {
+    let keys = key.split('.');
+
+    keys.forEach( key => {
+      obj = obj[key]; 
+    });
+
+    return obj;
   }
 }
